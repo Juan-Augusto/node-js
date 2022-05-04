8733bd5b233b0bb180ffddcc3c5cdbd141189815
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2022-05-03 22:58:09 -0300

    Nodemon and ts-node added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as Matematica from './Matematica';
-let n1: number = 10;
+let n1: number = 50;
 let n2: number = 20;
 console.log(`Soma: ${Matematica.Somar(n1, n2)}`);
 console.log(`Subtração: ${Matematica.Subtrair(n1, n2)}`);
