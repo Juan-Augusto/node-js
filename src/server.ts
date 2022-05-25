6bc325c35a006d2cc836890767f0a1112edb930e
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2022-05-25 08:43:26 -0300

    Routes added to the server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,13 @@ import express, {Request, Response} from 'express';
 
 const server = express();
 
-server.get('/', (req: Request, res: Response) => {
+server.get('/', (req: Request, res: Response ) => {
     res.send('Hello World!');
+})
+server.get('/teste', (req: Request, res: Response ) => {
+    res.send('Teste de rotas!');
+})
+server.get('/*', (req: Request, res: Response ) => {
+    res.send('404: not found');
 })
 server.listen(80);
