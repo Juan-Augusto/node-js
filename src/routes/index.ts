26d95ff30775ce67b11c72ab9266866c53052327
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2022-06-20 08:15:39 -0300

    Mustach page development

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,15 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-    res.render('home');
+    let user  = {
+        name: 'Juan Augusto',
+        age: '22',
+        email: 'juanaugusto1@main.com',
+        profile_pic: 'https://yt3.ggpht.com/yti/APfAmoFpCw3aOM6yumulCWvbKnuuC3quHD-qZei2NcSyjw=s88-c-k-c0x00ffffff-no-rj-mo'
+    };
+    res.render('home', {
+        user
+    });
 });
 
 router.get("/contato", (req: Request, res: Response) => {
