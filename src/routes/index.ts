fe7dacf3007d5663b7be055d0c62499add3d8c0f
Juan Augusto <83130876+Juan-Augusto@users.noreply.github.com>
2022-06-21 08:21:43 -0300

    Exhibition through one condition

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,14 +3,22 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
+    let userAge : number = 22;
+    let showAge : boolean = false;
+
+    if(userAge >= 18){
+        showAge = true;
+    }
+
     let user  = {
         name: 'Juan Augusto',
-        age: '22',
         email: 'juanaugusto1@main.com',
-        profile_pic: 'https://yt3.ggpht.com/yti/APfAmoFpCw3aOM6yumulCWvbKnuuC3quHD-qZei2NcSyjw=s88-c-k-c0x00ffffff-no-rj-mo'
+        age: 22,
+        profile_pic: 'https://yt3.ggpht.com/yti/APfAmoFpCw3aOM6yumulCWvbKnuuC3quHD-qZei2NcSyjw=s88-c-k-c0x00ffffff-no-rj-mo',
     };
     res.render('home', {
-        user
+        user,
+        showAge
     });
 });
 
